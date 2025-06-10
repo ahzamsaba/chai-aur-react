4a7fff15bb7b9b36afafb2dc2f3b0f870b47dd8e
ahzamsaba <ahzamsaba3052@gmail.com>
2025-06-10 11:44:36 +0530

    Create react projects with & without vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Chai aur React | Ahzam Saba</h1>
+      <h3>npx create-react-app 01basicreact</h3>
+      <p>First read package.json file then use only App.js and index.js and run 
+        npm run start
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Chai aur React with Vite | Ahzam Saba</h1>
+      <p>deleted unnecessary files and code to get to know react with vite</p>
+    </div>    
+  )
+}
+
+export default App
