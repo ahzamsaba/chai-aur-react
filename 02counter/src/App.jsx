3fdce36f78c67c0b830942d991d3ef5182d3b1e3
ahzamsaba <ahzamsaba3052@gmail.com>
2025-06-11 16:51:12 +0530

    batches & setCounter4times

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -9,10 +9,25 @@ function App() {
 
   // let counter = 5
   const increment = () => {
-    counter += 1
-    setCounter(counter)
-    console.log(counter)
+    setCounter(counter+1)
+  }  
+
+  const increment4 = () => {
+    setCounter(prevCounter => prevCounter+1)
+    setCounter(prevCounter => prevCounter+1)
+    setCounter(prevCounter => prevCounter+1)
+    setCounter(prevCounter => prevCounter+1)
   }
+//-> React batches multiple state updates together for performance reasons
+// counter = 15
+// setCounter(16)  // based on counter = 15
+// setCounter(16)  // still based on counter = 15
+// setCounter(16)  // again, based on counter = 15
+
+// You're telling React:
+// “Don't use the current value of counter from the outer scope. 
+//  Instead, give me the latest updated value at the time of this update.”
+//  setCounter(prevCounter => prevCounter+1)
 
   const decrement = () => {
     if(counter>0) counter -= 1
@@ -24,6 +39,7 @@ function App() {
       <h1>Chai aur React</h1>
       <h2>Counter value : {counter}</h2>
       <button onClick={increment}>Increment {counter}</button>
+      <button onClick={increment4}>Increment 4 times {counter}</button>
       <br />
       <button onClick={decrement}>Decrement {counter} </button>
       <p>footer : {counter} </p>
